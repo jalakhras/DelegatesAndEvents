1715c66f18c476d078412c05ab2659aa8998d23c
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-06-28 22:04:06 +0300

    Using Anonymous Methods

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -14,22 +14,26 @@ namespace DelegatesAndEvents
             //int finalHours = del1(5, WorkType.Golf);
             //Console.WriteLine("final Hours : " + finalHours.ToString());
             Worker worker = new Worker();
-            worker.WorkPerformed += Worker_WorkPerformed;
-            worker.WorkPerformed -= Worker_WorkPerformed;
-            worker.WorkCompleted += worker_WorkComplate;
+            worker.WorkPerformed +=delegate(object sender, WorkPerformedEventArgs e) {
+                Console.WriteLine("Houres Worked : " + e.Hours + " " + e.WorkType);
+            };
+            //worker.WorkPerformed -= Worker_WorkPerformed;
+            worker.WorkCompleted += delegate(object sender, EventArgs eventArgs) {
+                Console.WriteLine("Worker is Done");
+            };
             worker.DoWork(8, WorkType.GenerateReports);
         }
 
-        private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
-        {
-            Console.WriteLine("Houres Worked : " + e.Hours + " " + e.WorkType);
-        }
+        //private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        //{
+        //    Console.WriteLine("Houres Worked : " + e.Hours + " " + e.WorkType);
+        //}
 
         
-        static void worker_WorkComplate(object sender  , EventArgs eventArgs)
-        {
-            Console.WriteLine("Worker is Done");
-        }
+        //static void worker_WorkComplate(object sender  , EventArgs eventArgs)
+        //{
+        //    Console.WriteLine("Worker is Done");
+        //}
         //static void DoWork(WorkPerformedHandler del)
         //{
         //    del(5, WorkType.Golf);
